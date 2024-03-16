b65853e0264b023151f1eaf066490742098382ff
karanyadavv <karanyadav729@gmail.com>
2024-03-16 15:27:13 +0530

    Login & Register page added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,14 +2,19 @@ import './App.css'
 import { Route, Routes } from 'react-router-dom';
 import IndexPage from './pages/IndexPage';
 import LoginPage from './pages/LoginPage';
+import Layout from './Layout';
+import RegisterPage from './pages/RegisterPage';
 
 
 function App() {
 
   return (
     <Routes>
-      <Route index element={<IndexPage />} />
+      <Route path='/' element={<Layout />}>
+        <Route index element={<IndexPage />} />
+      </Route>
       <Route path='/login' element={<LoginPage />} />
+      <Route path='/register' element={<RegisterPage />} />
     </Routes>
     
   )
--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,19 +1,25 @@
 import { Link } from "react-router-dom";
 
-export default function LoginPage(){
+export default function RegisterPage(){
   return(
-    <div className="flex items-center justify-center mt-20">
-      <div className="w-full max-w-xs">
+    <div className="flex items-center justify-center mt-10">
+      <div className="w-full max-w-sm">
         <Link to={"/"} className="block text-primary text-2xl font-bold mb-4 pl-4" for="username">
           airbnb
         </Link>
         <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
         <div className="mb-4">
           <label className="block text-gray-700 text-xl font-bold mb-4" for="username">
-          Sign in to your account
+            Create your account
           </label>
           <label className="block text-gray-700 text-sm font-bold mb-2" for="username">
-            E-mail
+            Full name
+          </label>
+          <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="John Doe" />
+        </div>
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2" for="username">
+            Email
           </label>
           <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="your@email.com" />
         </div>
@@ -25,20 +31,17 @@ export default function LoginPage(){
           <p className="text-gray-500 text-xs italic">Use at least 8 characters, one uppercase, one lowercase and one number.</p>
         </div>
         <div className="flex items-center justify-between">
-          <button className="bg-[#E0565B] hover:bg-primary text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
-            Sign In
+          <button className="w-full bg-[#E0565B] hover:bg-primary text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+            Create account
           </button>
-          <a className="inline-block align-baseline font-bold text-sm text-gray-500 hover:text-gray-800" href="#">
-            Forgot Password?
-          </a>
         </div>
         </form>
         <div className="flex gap-2 text-gray-500 text-xs pl-4">
-        <p>Don't have an account?</p>
-        <Link to={"/register"} className="text-primary">Signup</Link>
+        <p>Already a member?</p>
+        <Link to={"/login"} className="text-primary">Signin</Link>
         </div>
         
       </div>
     </div>
-  );
+  )
 }
