39dca9af86c0cbf8f19c2fe15b3382b4eb783ba8
karanyadavv <karanyadav729@gmail.com>
2024-03-17 21:51:28 +0530

    Header mobile responsive

--- client/src/Header.jsx
+++ client/src/Header.jsx
@@ -9,14 +9,14 @@ export default function Header(){
           <img src={Logo} alt="" className='w-8 text-primary '/>
           <span className='font-bold text-xl text-primary'>airbnb</span>
         </Link>
-        <div className='flex items-center gap-4 border border-gray-300 rounded-full md:py-3 px-4 shadow-md shadow-gray-300 text-sm'>
-          <div>Anywhere</div>
-          <div className='border-l border-gray-300 h-6 sm:block'></div>
-          <div>Any week</div>
-          <div className='border-l border-gray-300 h-6 sm:block'></div>
-          <div className="">Add guests</div>
+        <div className='text-xs mr-10 flex items-center gap-2 md:gap-4 border border-gray-300 rounded-full md:py-3 px-4 shadow-md shadow-gray-300 md:text-sm'>
+          <div className="font-bold text-sm md:font-normal">Anywhere</div>
+          <div className='hidden border-l border-gray-300 h-6 sm:block'></div>
+          <div className="hidden sm:block">Any week</div>
+          <div className='hidden border-l border-gray-300 h-6 sm:block'></div>
+          <div className="hidden sm:block">Add guests</div>
           <button className='bg-primary text-white p-1 rounded-full'>
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="3" stroke="currentColor" class="w-4 h-4">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="3" stroke="currentColor" className="w-4 h-4 flex flex-row-reverse">
             <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
           </svg>
           </button>
